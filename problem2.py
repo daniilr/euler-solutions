fd2acebb77a801f1fbc032670aff7ad24050ce1e
Даниил Рыжков <daniil.re@gmail.com>
2012-08-07 20:13:37 +1100

    Now better

--- problem2.py
+++ problem2.py
@@ -2,10 +2,9 @@
 prev = 1
 preprev = 0
 sum = 0
-while True:
+fib = 0
+while fib >= 4000000:
     fib = prev + preprev
-    if fib >= 4000000:
-        break
     if fib % 2 == 0:
         sum += fib
     preprev = prev
